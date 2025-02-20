893b0c024ea5ac5b9bc9948e9e94438d45bbbdfc
Wallace027Dev <wallaceofc@hotmail.com>
2025-02-20 06:55:39 -0300

    ajustando formatacao de transacoes

--- src/utils/formatTransactions.ts
+++ src/utils/formatTransactions.ts
@@ -1,14 +1,16 @@
 import ITransaction from "@/interfaces/ITransaction";
 
 export const formatTransactions = (transactions: ITransaction[]) => {
-  let saldo = 0;
-  console.log(transactions);
-  return transactions.map((transaction) => {
-    saldo +=
-      transaction.type === "RECIPE" ? transaction.value : -transaction.value;
-    return {
-      date: new Date(transaction.createdAt).toLocaleDateString("pt-BR"),
-      saldo
-    };
-  });
+  console.log("Transações antes do formato:", transactions);
+
+  const formatted = transactions
+    .map((t: ITransaction) => ({
+      date: t.createdAt
+        ? new Date(t.createdAt).toISOString().split("T")[0]
+        : "Data Inválida",
+      saldo: t.value
+    }))
+    .sort((a, b) => a.date.localeCompare(b.date));
+
+  return formatted;
 };
